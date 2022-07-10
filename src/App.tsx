1d579028880f4e352afae2efb11199ad17bba39e
ubungmeister <ubungmeister@gmail.com>
2022-07-10 17:09:14 +0200

    Callbacks/controlled vs uncontrolled state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Accordion} from "./componenty/Accordion/Accordion";
+import {Rating, RatingValueType} from "./componenty/Rating/Rating";
+import {Pagetitle} from "./componenty/PageTitle/Pagetitle";
+import {OnOff} from "./componenty/onOff/OnOff";
+import {UncontrolledAccordion} from "./componenty/Accordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./componenty/Rating/UncontrolledRating";
+import {UncontrolledRating2} from "./componenty/Rating/UncontrolledRating2";
+import {Rating_varian2} from "./componenty/Rating/Rating_varian2";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [ratingValue, setRatingValue] =useState<RatingValueType>(0)
+    let [accardionValue, setAccardionValue] = useState(true)
+    let[state,newState] = useState(false)
+
+    return (<div>
+    {/*<Rating_varian2 value={ratingValue} onClick={setRatingValue}/>*/}
+        <Accordion titleValue={'hello'} collapsed={accardionValue} onClick={setAccardionValue}/>
+        <OnOff state={state} newState={newState}/>
+
+    </div>)
 }
 
 export default App;
+
